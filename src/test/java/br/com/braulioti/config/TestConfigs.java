1ac9416552164b4eb835b6299e95ecb1ecf0b36d
Bráulio Figueiredo <braulio@braulioti.com.br>
2026-02-18 12:37:52 -0300

    Finalizando o Script de Continuous Integration e Delivery com Github Actions

--- src/test/java/br/com/braulioti/config/TestConfigs.java
+++ src/test/java/br/com/braulioti/config/TestConfigs.java
@@ -5,7 +5,7 @@ public interface TestConfigs {
 
     String HEADER_PARAM_AUTHORIZATION = "Authorization";
     String HEADER_PARAM_ORIGIN = "Origin";
-    String ORIGIN_LOCALHOST = "http://localhost:9000";
+    String ORIGIN_LOCALHOST = "http://localhost:8888";
     String ORIGIN_BRAU_IO = "https://brau.io";
     String ORIGIN_BRAULIO_TI = "https://braulioti.com.br";
 }
